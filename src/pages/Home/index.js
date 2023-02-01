2204d8023f992525e9168ba8f39418dc90093798
mdsaputra <mdsaputraa@gmail.com>
2023-02-02 00:35:25 +0700

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -18,7 +18,7 @@ const Home = () => {
           <BoxAds ads="pubg" />
         </View>
       </ScrollView>
-      <Text style={styles.judul}>Mobile Game</Text>
+      <Text style={styles.judul}>Mobile Gameee</Text>
     </View>
   );
 };
